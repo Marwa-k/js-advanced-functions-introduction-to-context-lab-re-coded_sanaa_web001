d587f23df05b1172d86e6d8e4465a60dfabc7b12
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-18 13:12:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let createEmployeeRecord = function (data) {
+    return {
+        firstName: data[0],
+        lastName: data[1],
+        title: data[2],
+        payPerHour: info[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployees(arr) {
+  return arr.map(createEmployeeRecord);
+}
+
+
+let createTimeOutEvent = function (employeeRecord, dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return employeeRecord
+}
+
+
+
